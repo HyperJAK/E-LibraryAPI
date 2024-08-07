74276c8903070025e1adb4e970b4dcb93f50ddd2
HyperJAK <james.jakthesamething@gmail.com>
2024-08-07 14:30:03 +0300

    Created Default project (with 1 additional class)

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,47 @@
+﻿namespace ELib_IDSFintech_Internship.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StudentSystemContext context)
+        {
+            if (context.Students.Any()
+                && context.Locations.Any()
+                /*&& context.Tasks.Any()
+                && context.SubTasks.Any()*/)
+            {
+                return;   //DB has been seeded
+            }
+
+            var location = new Location
+            {
+                Id = 1,
+                City = "Beirut",
+                Country = "Lebanon",
+                Address = "First street idk what",
+                TimeStamp = DateTime.Now,
+
+            };
+
+            var student = new Student
+            {
+                FirstName = "James",
+                LastName = "AK",
+                Email = "user1@example.com",
+                Location = location,
+                LocationId = location.Id,
+                TimeStamp = DateTime.Now,
+            };
+
+            location.Students = new List<Student> { student };
+
+
+            // Add entities to context
+            context.Students.Add(student);
+            context.Locations.Add(location);
+
+
+            // Save changes to database
+            context.SaveChanges();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# E-Library API
+  API created using C# ASP framework, showcasing Dependency injection, Repository pattern, and entity framework
+
+# Latest Release
+  (#)
+  
+## App Production showcase
+  # (Img)
+
+# External Licenses
+  ## (Source Name)  
+  # (Source license)
+
+  ## (Source Name)
+  # (Source license)
+
+
+
