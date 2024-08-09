62981f38337c375b9926673b7d1ebadb9ebbfcb3
HyperJAK <james.jakthesamething@gmail.com>
2024-08-09 20:56:33 +0300

    Created a default globally used repository for the default CRUD Operations (GET POST UPDATE DELETE)

---
+++ Repositories/IDefaultRepository.cs
@@ -0,0 +1,17 @@
+﻿using ELib_IDSFintech_Internship.Models.Books;
+
+namespace ELib_IDSFintech_Internship.Repositories
+{
+    public interface IDefaultRepository
+    {
+        public Task<IEnumerable<BookAuthor>?> GetAll();
+
+        public Task<BookAuthor?> GetById(int ID);
+
+        public Task<int?> Create(BookAuthor entity);
+
+        public Task<int?> Update(BookAuthor newEntity);
+
+        public Task<int?> Delete(int ID);
+    }
+}
